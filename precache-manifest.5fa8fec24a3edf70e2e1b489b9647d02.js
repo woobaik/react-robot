3710edc8fb2716eb82227c2a3a352a65a7377994
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-11-07 21:15:06 -0800

    Updates

---
+++ precache-manifest.5fa8fec24a3edf70e2e1b489b9647d02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abaac0f8aa9832205c7de5fabc64549b",
+    "url": "/react-robot/index.html"
+  },
+  {
+    "revision": "93f402e760739455913b",
+    "url": "/react-robot/static/css/main.4c139018.chunk.css"
+  },
+  {
+    "revision": "9ee6939c22a8eb592d45",
+    "url": "/react-robot/static/js/2.ea1d0765.chunk.js"
+  },
+  {
+    "revision": "93f402e760739455913b",
+    "url": "/react-robot/static/js/main.8ee8e6b3.chunk.js"
+  },
+  {
+    "revision": "21366922b74f2411b85b",
+    "url": "/react-robot/static/js/runtime-main.dff651f6.js"
+  }
+]);
